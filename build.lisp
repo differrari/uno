0cd104b8612ca1693306b78226b15b197925784e
di <git@diferrari.com>
2026-07-19 00:00:00 +0000

    [BUILD] dyn-target for better cross comp

--- build.lisp
+++ build.lisp
@@ -5,6 +5,6 @@
 (redbuild:quick-build (redbuild:make-instance `redbuild:redmod
         :name "uno"
         :type (redbuild:dynlib)
-        :target (redbuild:native)
+        :target (redbuild:dyn-target)
         :srcs (redbuild:dynsrc "doc.c" "text.c" "uno.c")
 ) :add-dependencies t :run t :success (lambda () (print (redbuild:emit-compile-commands))))
